e96288a4e42ad926783f7a275561cebdb7403d79
trySometngNew <trySometngNew@users.noreply.github.com>
2019-01-28 21:15:45 +0530

    promise Async function call
    promise Async function call

--- playground/promise.js
+++ playground/promise.js
@@ -1,13 +1,31 @@
-var somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve('Hey it worked!');
-        reject('Unable to fulfill promise');
-    }, 2500);
-    
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(12, 24).then((result) => {
+    console.log('Result: ', result);
+}, (errorMessage) => {
+    console.log(errorMessage)
 });
 
-somePromise.then((message) => {
-    console.log('Success: ', message);
-}, (errorMessage) => {
-    console.log('Error: ', errorMessage);
-});
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve('Hey it worked!');
+//         reject('Unable to fulfill promise');
+//     }, 2500);
+    
+// });
+
+// somePromise.then((message) => {
+//     console.log('Success: ', message);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
